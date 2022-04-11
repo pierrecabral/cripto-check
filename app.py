8af11958250d4e2e71e84288dabbe69acde406fd
Pierre Cabral <pierrecabral@gmail.com>
2022-04-12 08:43:24 +1000

    Mock test done

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, Response
 import requests
 import json
+import os
+
 
 app = Flask(__name__)
+mode = os.environ.get('app_mode')
+if mode == "test":
+    base_url = "http://127.0.0.1:5000/mock/"
+else:
+    base_url = "https://api.coinbase.com/v2/prices/spot?currency="
 
 
 @app.route('/')
@@ -12,7 +19,6 @@ def home():
 
 @app.route('/shortenurl', methods=['GET', 'POST'])
 def shortenurl():
-    base_url = "https://api.coinbase.com/v2/prices/spot?currency="
     url = ''.join([base_url, request.form['shortcode']])
     response = requests.get(url)
     data = response.json()
@@ -23,37 +29,33 @@ def shortenurl():
 
 @app.route('/EUR')
 def EUR():
-    response = requests.get(
-        'https://api.coinbase.com/v2/prices/spot?currency=EUR')
+    url = ''.join([base_url, "EUR"])
+    response = requests.get(url)
     data = response.json()
-    # print(data)
     return data
 
 
 @app.route('/GBP')
 def GBP():
-    response = requests.get(
-        'https://api.coinbase.com/v2/prices/spot?currency=GBP')
+    url = ''.join([base_url, "GBP"])
+    response = requests.get(url)
     data = response.json()
-    # print(data)
     return data
 
 
 @app.route('/USD')
 def USD():
-    response = requests.get(
-        'https://api.coinbase.com/v2/prices/spot?currency=USD')
+    url = ''.join([base_url, "USD"])
+    response = requests.get(url)
     data = response.json()
-    # print(data)
     return data
 
 
 @app.route('/JPY')
 def JPY():
-    response = requests.get(
-        'https://api.coinbase.com/v2/prices/spot?currency=JPY')
+    url = ''.join([base_url, "JPY"])
+    response = requests.get(url)
     data = response.json()
-    # print(data)
     return data
 
 
@@ -61,6 +63,33 @@ def JPY():
 def health():
     status_code = Response(status=200)
     return status_code
+
+# Mock legit response
+
+
+@app.route('/mock/EUR')
+def mock_EUR():
+    response = '{"data":{"base":"BTC","currency":"EUR","amount":"37735.92"}}'
+    print(response)
+    return response
+
+# Mock Wrong Json
+
+
+@app.route('/mock/GBP')
+def mock_GBP():
+    response = 'wrong json object'
+    print(response)
+    return response
+
+# Mock Invalid Currency
+
+
+@app.route('/mock/ZZZ')
+def mock_ZZZ():
+    response = '{"errors":[{"id":"invalid_request","message":"Currency is invalid"}]}'
+    print(response)
+    return response
 
 
 if __name__ == '__main__':
